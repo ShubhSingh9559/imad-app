e80d93a9ca4aada8acade7a4eefa4d67441aca44
ShubhSingh9559 <singhshubham102@gmail.com>
2017-08-07 20:49:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+//Code for counter
+var counter= document.getElementById('counter');
+button.onclick= function(){
+
+    //Create request for counter endpoint
+    var request= new XMLHttpRequest();
+    //Capture response and store it in variable
+    request.onreadystatechnage=function(){
+    if (request.readyState ===XMLHttprequest.DONE){
+        //take action
+        if(request.status === 200){
+            var counter=request.responeText;
+            var span=document.getElementByID('count');
+            span.InnerHTML=counter.toString();
+            
+        }
+    }
+    };
+        
+    //Make request
+    request.open('GET','http://singhshubham102.imad.hasura-app.io/counter');
+    request=send(null);
+    
+}
